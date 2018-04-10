0a8a85628a594f4c261cc1083ce2af2ad5206602
AlejandroVC67 <vcalejandro97@gmail.com>
2018-04-09 23:05:32 -0500

    generating automatic sprites

---
+++ AngularPokedex/sprite-config.js
@@ -0,0 +1,15 @@
+const replace = require('replace-in-file');
+
+  const options = {
+    files: './src/styles/_sprites.scss',
+    from: '../',
+    to: '../../',
+  };
+  try {
+    const changes = replace.sync(options);
+    console.log('Modified files:', changes.join(', '));
+
+  }
+  catch (error) {
+    console.error('Error occurred:', error);
+  }
