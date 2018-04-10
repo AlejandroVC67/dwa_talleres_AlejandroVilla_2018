0a6533040e967a644e496711d57c7f4d5bed1b24
AlejandroVC67 <vcalejandro97@gmail.com>
2018-04-09 22:13:05 -0500

    finished footer

--- AngularPokedex/src/app/app.module.ts
+++ AngularPokedex/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PokemonComponent
+    PokemonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
